df33b0cd1a40feeab37f045972acd423d4b5789a
mmistika <mistikasoft@gmail.com>
2025-02-02 15:36:40 +0100

    Orwell solution

--- mooc-java-programming-i/part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ mooc-java-programming-i/part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -7,5 +7,10 @@ public class Orwell {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a number: ");
+        int value = Integer.valueOf(scan.nextLine());
+        if (value == 1984) {
+            System.out.println("Orwell");
+        }
     }
 }
