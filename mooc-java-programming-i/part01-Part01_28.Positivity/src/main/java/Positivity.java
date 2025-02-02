72ccc44b886227d43d8a6e2d93c3d2f1f051912f
mmistika <mistikasoft@gmail.com>
2025-02-02 15:41:48 +0100

    Positivity solution

--- mooc-java-programming-i/part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ mooc-java-programming-i/part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -7,5 +7,13 @@ public class Positivity {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a number: ");
+        int value = Integer.valueOf(scan.nextLine());
+        
+        if (value > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
     }
 }
