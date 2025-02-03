262a681b58c2587cc8fc5ab731d53c3194740130
mmistika <mistikasoft@gmail.com>
2025-02-03 18:14:00 +0100

    CountingToHundred solution

--- mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,9 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int value = Integer.valueOf(scanner.nextLine());
+        for (int i = value; i <= 100; ++i) {
+            System.out.println(i);
+        }
     }
 }
