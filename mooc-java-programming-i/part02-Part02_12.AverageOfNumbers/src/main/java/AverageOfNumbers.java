de81cd7ac045368c3c62a94622f85cff11844fa6
mmistika <mistikasoft@gmail.com>
2025-02-03 17:51:44 +0100

    AverageOfNumbers solution

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,19 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int value = Integer.valueOf(scanner.nextLine());
+            
+            if (value == 0) {
+                break;
+            }
+            
+            count = count + 1;
+            sum = sum + value;
+        }
+        System.out.println("Average of the numbers: " + ((double)sum / count));
     }
 }
