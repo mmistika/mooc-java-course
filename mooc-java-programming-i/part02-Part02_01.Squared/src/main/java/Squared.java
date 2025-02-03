1d2fca9ba622a267de2aed978449b23fe184fd77
mmistika <mistikasoft@gmail.com>
2025-02-03 16:44:07 +0100

    Squared solution

--- mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
+++ mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,9 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int value = Integer.valueOf(scanner.nextLine());
+        
+        int square = value * value;
+        System.out.println(square);
     }
 }
