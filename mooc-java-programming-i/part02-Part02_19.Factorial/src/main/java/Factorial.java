0461ff08e5aa47e6e4da1c21e8fd54f80ee874fd
mmistika <mistikasoft@gmail.com>
2025-02-03 18:41:33 +0100

    Factorial solution

--- mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,16 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number:");
+        int value = Integer.valueOf(scanner.nextLine());
+
+        int factorial = 1;
+        if (value != 0) {
+            for (int i = 2; i <= value; ++i) {
+                factorial = factorial * i;
+            }
+        }
+
+        System.out.println("Factorial: " + factorial);
     }
 }
