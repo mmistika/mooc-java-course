422221236d2113444c45a141b6893b389adf3f82
mmistika <mistikasoft@gmail.com>
2025-02-06 23:11:58 +0100

    Division solution

--- mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
+++ mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
@@ -12,4 +12,7 @@ public class Division {
     }
 
     // implement the method here
+    public static void division(int numerator, int denominator) {
+        System.out.println((double)numerator / denominator);
+    }
 }
