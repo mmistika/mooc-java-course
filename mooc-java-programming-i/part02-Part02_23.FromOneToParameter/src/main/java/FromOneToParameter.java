9120318e6cd48de97ac52fbec609625b41d183f2
mmistika <mistikasoft@gmail.com>
2025-02-06 23:06:41 +0100

    FromOneToParameter solution

--- mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -6,4 +6,9 @@ public class FromOneToParameter {
 
     }
 
+    public static void printUntilNumber(int number) {
+        for (int i = 1; i <= number; ++i) {
+            System.out.println(i);
+        }
+    }
 }
