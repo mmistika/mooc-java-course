f6e34e1bcc5917961c96a6e9696bd83d6872bdf5
mmistika <mistikasoft@gmail.com>
2025-02-07 21:41:31 +0100

    IndexOf solution

--- mooc-java-programming-i/part03-Part03_10.IndexOf/src/main/java/IndexOf.java
+++ mooc-java-programming-i/part03-Part03_10.IndexOf/src/main/java/IndexOf.java
@@ -20,5 +20,14 @@ public class IndexOf {
         System.out.println("");
 
         // implement here finding the indices of a number
+        System.out.println("Search for?");
+        int search = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = 0; i < list.size(); ++i) {
+            int num = list.get(i);
+            if (num == search) {
+                System.out.println(search + " is at index " + i);
+            }
+        }
     }
 }
