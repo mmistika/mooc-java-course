ed3b2aae9e6c66e86a762de4f8b4bb81ce02fb74
mmistika <mistikasoft@gmail.com>
2025-02-07 21:38:33 +0100

    GreatestInList solution

--- mooc-java-programming-i/part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
+++ mooc-java-programming-i/part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -16,9 +16,19 @@ public class GreatestInList {
 
             list.add(input);
         }
-        
+
         System.out.println("");
 
         // implement finding the greatest number in the list here
+        int greatest = list.get(0);
+        
+        for (int i = 1; i < list.size(); ++i) {
+            int num = list.get(i);
+            if (greatest < num) {
+                greatest = num;
+            }
+        }
+
+        System.out.println("The greatest number: " + greatest);
     }
 }
