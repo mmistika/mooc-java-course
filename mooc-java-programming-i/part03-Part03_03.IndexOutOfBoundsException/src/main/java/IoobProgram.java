64eba44b8c5addc5dc57ae4565b15ae698354785
mmistika <mistikasoft@gmail.com>
2025-02-07 21:19:20 +0100

    IoobProgram solution

--- mooc-java-programming-i/part03-Part03_03.IndexOutOfBoundsException/src/main/java/IoobProgram.java
+++ mooc-java-programming-i/part03-Part03_03.IndexOutOfBoundsException/src/main/java/IoobProgram.java
@@ -7,6 +7,7 @@ public class IoobProgram {
         // Modify this program to cause an indexOutOfBoundsException 
 
         ArrayList<String> lines = new ArrayList<>();
+        lines.get(0);
         lines.add("Never has a man influenced physics so profoundly as Niels Bohr in the early 1900's");
         lines.add("Going back to this time period, little was known about atomic structure; Bohr set out");
         lines.add("to end the obscurity of physics. However, things didn't come easy for Bohr. He had to");
