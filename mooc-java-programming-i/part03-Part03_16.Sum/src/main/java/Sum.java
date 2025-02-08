6f76701e8fc43b6a2537a2932a4cad6b03ba3522
mmistika <mistikasoft@gmail.com>
2025-02-08 15:07:25 +0100

    Sum solution

--- mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
+++ mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
@@ -7,4 +7,11 @@ public class Sum {
         // Try your method here
     }
 
+    public static int sum(ArrayList<Integer> numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum = sum + num;
+        }
+        return sum;
+    }
 }
