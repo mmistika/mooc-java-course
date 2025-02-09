46f3048e4e4721b450b76ed3f0ddcd58a070d3cf
mmistika <mistikasoft@gmail.com>
2025-02-09 17:38:32 +0100

    PersonalDetails solution

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,29 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        double yearsSum = 0;
+        int yearsCount = 0;
+        String longestName = "";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] parts = input.split(",");
+            String name = parts[0];
+            int year = Integer.valueOf(parts[1]);
+            
+            yearsSum = yearsSum + year;
+            ++yearsCount;
+            
+            if (longestName.length() < name.length()) {
+                longestName = name;
+            }
+        }
+        
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + (yearsSum / yearsCount));
     }
 }
