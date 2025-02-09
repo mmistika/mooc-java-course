f48504ff68baec13cde36f0cda9685c691d58474
mmistika <mistikasoft@gmail.com>
2025-02-09 17:01:01 +0100

    IsItTrue solution

--- mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
+++ mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
@@ -6,5 +6,13 @@ public class IsItTrue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a string: ");
+        boolean value = Boolean.valueOf(scanner.nextLine());
+        
+        if (value) {
+            System.out.println("You got it right!");
+        } else {
+            System.out.println("Try again!");
+        }
     }
 }
