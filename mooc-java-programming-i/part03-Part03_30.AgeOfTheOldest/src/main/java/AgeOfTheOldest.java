b364fb12024ef2ee78bf6f60f85e8ff21ceea8bc
mmistika <mistikasoft@gmail.com>
2025-02-09 17:26:06 +0100

    AgeOfTheOldest solution

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,20 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int oldest = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            
+            int age = Integer.valueOf(input.split(",")[1]);
+            if (oldest < age) {
+                oldest = age;
+            }
+        }
+        
+        System.out.println("Age of the oldest: " + oldest);
     }
 }
