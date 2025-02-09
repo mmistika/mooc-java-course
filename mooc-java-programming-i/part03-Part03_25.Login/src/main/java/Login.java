fed19893ad86c3fe19951df2eeff838546f09528
mmistika <mistikasoft@gmail.com>
2025-02-09 17:10:52 +0100

    Login solution

--- mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -6,5 +6,27 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] usernames = {"alex", "emma"};
+        String[] passwords = {"sunshine", "haskell"};
+        
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        
+        boolean success = false;
+        for (int i = 0; i < usernames.length; ++i) {
+            if (usernames[i].equals(username) && passwords[i].equals(password)) {
+                success = true;
+                break;
+            }
+        }
+        
+        if (success) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
