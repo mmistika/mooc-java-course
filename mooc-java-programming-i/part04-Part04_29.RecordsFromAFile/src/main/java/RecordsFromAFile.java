3325ab1225b30f78b5803aa36a2ed0b2d66f63e8
mmistika <mistikasoft@gmail.com>
2025-02-10 20:04:47 +0100

    RecordsFromAFile solution

--- mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -7,5 +7,26 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the file:");
+        String fileName = scanner.nextLine();
+        
+        try (Scanner file = new Scanner(Paths.get(fileName))) {
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                System.out.print(name + ", age: " + age + " year");
+                if (age > 1 || age == 0) {
+                    System.out.println("s");
+                } else {
+                    System.out.println("");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
