8ff06b9c2951ab95e90c6581dfbb6da61af1ec42
mmistika <mistikasoft@gmail.com>
2025-02-10 15:51:27 +0100

    Books solution

--- mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,36 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        
+        ArrayList<Book> books = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+            if (title.equals("")) {
+                break;
+            }
+            
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            
+            System.out.print("Publication year: ");
+            int year = Integer.valueOf(scanner.nextLine());
+            
+            books.add(new Book(title, pages, year));
+        }
+        System.out.println("");
+        
+        System.out.print("What information will be printed? ");
+        String type = scanner.nextLine();
+        
+        for (Book book : books) {
+            if (type.equals("name")) {
+                System.out.println(book.getTitle());
+            } else {
+                System.out.println(book);
+            }
+        }
     }
 }
