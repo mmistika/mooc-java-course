1d30c9c761b726832887a9adcceb82c3157590ea
mmistika <mistikasoft@gmail.com>
2025-02-10 15:36:26 +0100

    PersonalInformation solution

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,25 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+            if (firstName.equals("")) {
+                break;
+            }
+            
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+            
+            System.out.print("Identification number: ");
+            String number = scanner.nextLine();
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, number));
+        }
+        System.out.println("");
+       
+        for (PersonalInformation info : infoCollection) {
+            System.out.println(info.getFirstName() + " " + info.getLastName());
+        }
     }
 }
