5354a9aa2ad6a26bbee38adb8e4387bc1107ec5f
mmistika <mistikasoft@gmail.com>
2025-02-10 20:21:45 +0100

    StoringRecords solution

--- mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -25,7 +25,20 @@ public class StoringRecords {
 
         // Write here the code for reading from file
         // and printing the read records
-        return persons;
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
 
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                persons.add(new Person(name, age));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        return persons;
     }
 }
