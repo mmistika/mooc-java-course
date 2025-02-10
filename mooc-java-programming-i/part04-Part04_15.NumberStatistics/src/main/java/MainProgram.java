c6a9958d8f127e0f82ebb75c94d400b64acee5ce
mmistika <mistikasoft@gmail.com>
2025-02-10 15:07:18 +0100

    NumberStatistics solution

--- mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,29 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        
+        Statistics statAll = new Statistics();
+        Statistics statEven = new Statistics();
+        Statistics statOdd = new Statistics();
+        
+        System.out.println("Enter numbers:");
+        
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            
+            statAll.addNumber(input);
+            if (input % 2 == 0) {
+                statEven.addNumber(input);
+            } else {
+                statOdd.addNumber(input);
+            }
+        }
+        
+        System.out.println("Sum: " + statAll.sum());
+        System.out.println("Sum of even numbers: " + statEven.sum());
+        System.out.println("Sum of odd numbers: " + statOdd.sum());
     }
 }
