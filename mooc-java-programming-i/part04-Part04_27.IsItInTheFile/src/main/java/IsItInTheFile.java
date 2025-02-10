7482a3684d8133a378574e5c8546379b5de0482d
mmistika <mistikasoft@gmail.com>
2025-02-10 19:49:15 +0100

    IsItInTheFile solution

--- mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -12,6 +12,23 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
-
+        
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            boolean found = false;
+            
+            while (fileScanner.hasNextLine()) {
+                if (fileScanner.nextLine().equals(searchedFor)) {
+                    found = true;
+                }
+            }
+            
+            if (found) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found!");
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
     }
 }
