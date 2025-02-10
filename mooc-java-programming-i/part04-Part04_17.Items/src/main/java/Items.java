70dce2d76ab9756a4d0acf586c72566c84852eba
mmistika <mistikasoft@gmail.com>
2025-02-10 15:30:26 +0100

    Items solution

--- mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
+++ mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
@@ -10,5 +10,19 @@ public class Items {
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            if (name.equals("")) {
+                break;
+            }
+            
+            items.add(new Item(name));
+        }
+        System.out.println("");
+        
+        for (Item item : items) {
+            System.out.println(item.toString());
+        }
     }
 }
