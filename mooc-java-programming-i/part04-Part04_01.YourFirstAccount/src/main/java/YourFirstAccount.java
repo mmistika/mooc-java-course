cf54e4f4448f12aedb6d72a7aedcf55be41a9932
mmistika <mistikasoft@gmail.com>
2025-02-10 13:48:18 +0100

    YourFirstAccount solution

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,10 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account acc = new Account("Owner", 100);
+
+        acc.deposit(20);
+
+        System.out.println(acc);
     }
 }
