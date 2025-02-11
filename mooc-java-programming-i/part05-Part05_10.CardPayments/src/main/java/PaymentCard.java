bf6c45730cca9fbe1d153757a7bc078041d28d43
mmistika <mistikasoft@gmail.com>
2025-02-11 15:31:08 +0100

    CardPayments solution

--- mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentCard.java
+++ mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentCard.java
@@ -19,6 +19,10 @@ public class PaymentCard {
         // implement the method so that it only takes money from the card if
         // the balance is at least the amount parameter.
         // returns true if successful and false otherwise
-        return false;
+        boolean canTake = this.balance >= amount;
+        if (canTake) {
+            this.balance = this.balance - amount;
+        }
+        return canTake;
     }
 }
--- mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,30 +1,72 @@
 
 public class PaymentTerminal {
 
-
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
-
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double affordablePrice = 2.50;
+        if (payment < affordablePrice) {
+            return payment;
+        }
+
+        ++this.affordableMeals;
+        this.money = this.money + affordablePrice;
+        return payment - affordablePrice;
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double heartyPrice = 4.30;
+        if (payment < heartyPrice) {
+            return payment;
+        }
+
+        ++this.heartyMeals;
+        this.money = this.money + heartyPrice;
+        return payment - heartyPrice;
     }
 
+    public boolean eatAffordably(PaymentCard card) {
+        // an affordable meal costs 2.50 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
+        boolean success = card.takeMoney(2.50);
+        if (success) {
+            ++this.affordableMeals;
+        }
+        return success;
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        // a hearty meal costs 4.30 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
+        boolean success = card.takeMoney(4.30);
+        if (success) {
+            ++this.heartyMeals;
+        }
+        return success;
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum < 0) {
+            return;
+        }
+        card.addMoney(sum);
+        this.money = this.money + sum;
+    }
 
     @Override
     public String toString() {
