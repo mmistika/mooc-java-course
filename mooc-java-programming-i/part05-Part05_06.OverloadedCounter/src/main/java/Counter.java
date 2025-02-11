640f79ed18fe814d0a6c2e679c304496b30aceb4
mmistika <mistikasoft@gmail.com>
2025-02-11 15:04:00 +0100

    OverloadedCounter solution

---
+++ mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
@@ -0,0 +1,38 @@
+
+public class Counter {
+    private int value;
+    
+    public Counter() {
+        this(0);
+    }
+    
+    public Counter(int startValue) {
+        this.value = startValue;
+    }
+    
+    public int value() {
+        return this.value;
+    }
+    
+    public void increase() {
+        this.increase(1);
+    }
+    
+    public void increase(int increaseBy) {
+        if (increaseBy < 0) {
+            return;
+        }
+        this.value = this.value + increaseBy;
+    }
+    
+    public void decrease() {
+        this.decrease(1);
+    }
+    
+    public void decrease(int decreaseBy) {
+        if (decreaseBy < 0) {
+            return;
+        }
+        this.value = this.value - decreaseBy;
+    }
+}
