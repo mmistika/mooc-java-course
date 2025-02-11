74e54e55f038530ee9b86e15d70a98d95e96e551
mmistika <mistikasoft@gmail.com>
2025-02-11 18:57:38 +0100

    HeightOrder solution

---
+++ mooc-java-programming-i/part06-Part06_07.HeightOrder/src/main/java/Room.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+
+public class Room {
+    private ArrayList<Person> people;
+    
+    public Room() {
+        this.people = new ArrayList<>();
+    }
+    
+    public void add(Person person) {
+        this.people.add(person);
+    }
+    
+    public boolean isEmpty() {
+        return this.people.isEmpty();
+    }
+    
+    public ArrayList<Person> getPersons() {
+        return this.people;
+    }
+    
+    public Person shortest() {
+        if (this.people.isEmpty()) {
+            return null;
+        }
+        
+        Person shortest = this.people.get(0);
+        for (Person person : this.people) {
+            if (shortest.getHeight() > person.getHeight()) {
+                shortest = person;
+            }
+        }
+        
+        return shortest;
+    }
+    
+    public Person take() {
+        Person shortest = this.shortest();
+        this.people.remove(shortest);
+        return shortest;
+    }
+}
