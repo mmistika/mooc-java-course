d3bfaf4eb0bab2c482d6d34d9986b029eb6a989c
mmistika <mistikasoft@gmail.com>
2025-02-11 15:42:06 +0100

    ComparingApartments solution

--- mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,27 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared) {
+        return this.squares > compared.squares;
+    }
+    
+    public int priceDifference(Apartment compared) {
+        int thisPrice = this.pricePerSquare * this.squares;
+        int comparedPrice = compared.pricePerSquare * compared.squares;
+        return Math.abs(thisPrice - comparedPrice);
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        int thisPrice = this.pricePerSquare * this.squares;
+        int comparedPrice = compared.pricePerSquare * compared.squares;
+        return thisPrice - comparedPrice > 0;
+    }
 }
