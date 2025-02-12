b6691d3fd9ebb438382e62f4ebc7f80dc034c3a0
mmistika <mistikasoft@gmail.com>
2025-02-12 19:17:56 +0100

    Searching solution

--- mooc-java-programming-i/part07-Part07_05.Searching/src/main/java/Searching.java
+++ mooc-java-programming-i/part07-Part07_05.Searching/src/main/java/Searching.java
@@ -44,10 +44,32 @@ public class Searching {
     }
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
+        for (int i = 0; i < books.size(); ++i) {
+            if (books.get(i).getId() == searchedId) {
+                return i;
+            }
+        }
         return -1;
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
+        int start = 0;
+        int end = books.size() - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int midId = books.get(mid).getId();
+            
+            if (midId == searchedId) {
+                return mid;
+            } else {
+                if (midId < searchedId) {
+                    start = mid + 1;
+                } else {
+                    end = mid -1;
+                }
+            }
+        }
         return -1;
     }
 }
