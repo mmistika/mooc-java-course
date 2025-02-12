c6185abd7eaf76e3a48027c72026b2d447ae189a
mmistika <mistikasoft@gmail.com>
2025-02-12 23:06:14 +0100

    Nicknames solution

--- mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,12 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> mapping = new HashMap<>();
+        mapping.put("matthew", "matt");
+        mapping.put("michael", "mix");
+        mapping.put("arthur", "artie");
+        
+        System.out.println(mapping.get("matthew"));
     }
 
 }
