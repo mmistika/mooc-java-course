8b674fe952afdb931dcebe01eb2803a2645ae978
mmistika <mistikasoft@gmail.com>
2025-02-12 23:51:03 +0100

    HashedDate solution

--- mooc-java-programming-ii/part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
+++ mooc-java-programming-ii/part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
@@ -50,5 +50,9 @@ public class SimpleDate {
 
         return this.year - other.year - yearRemoved;
     }
-
+    
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
 }
