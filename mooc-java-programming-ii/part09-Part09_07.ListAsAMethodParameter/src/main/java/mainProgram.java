88d3e4adb21fccfd6902270cea6d6325d999db34
mmistika <mistikasoft@gmail.com>
2025-02-15 00:21:41 +0100

    ListAsAMethodParameter solution

--- mooc-java-programming-ii/part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
+++ mooc-java-programming-ii/part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
@@ -1,4 +1,5 @@
 
+import java.util.List;
 
 public class mainProgram {
 
@@ -10,4 +11,7 @@ public class mainProgram {
     // Implement here a method returnSize
     // which returns the size of the list given to it
     //as a parameter
+    public static int returnSize(List<String> list) {
+        return list.size();
+    }
 }
