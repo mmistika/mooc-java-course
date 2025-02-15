864076ff39d8036592c47e8702a68ac269008403
mmistika <mistikasoft@gmail.com>
2025-02-15 15:02:53 +0100

    AverageOfSelectedNumbers solution

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -7,6 +7,26 @@ public class AverageOfSelectedNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // toteuta ohjelmasi tänne
-
+        ArrayList<String> inputs = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            
+            inputs.add(input);
+        }
+        System.out.println("");
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        boolean positive = scanner.nextLine().equals("p");
+        
+        double avg = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(num -> num > 0 == positive)
+                .average()
+                .getAsDouble();
+        
+        String optStr = positive ? "positive" : "negative";
+        System.out.println("Average of the " + optStr + " numbers: " + avg);
     }
 }
