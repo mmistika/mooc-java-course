692337f3a30a937a736a98357ff369f132a913d5
mmistika <mistikasoft@gmail.com>
2025-02-15 16:04:30 +0100

    LimitedNumbers solution

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -7,5 +7,18 @@ public class LimitedNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num < 0) {
+                break;
+            }
+
+            list.add(num);
+        }
+
+        list.stream()
+                .filter(num -> 1 <= num && num <= 5)
+                .forEach(System.out::println);
     }
 }
