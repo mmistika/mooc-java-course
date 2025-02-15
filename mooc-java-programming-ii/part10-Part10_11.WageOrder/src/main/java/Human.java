3352ddd291babfdbcccf62e76f44475cb7bbf186
mmistika <mistikasoft@gmail.com>
2025-02-15 17:04:49 +0100

    WageOrder solution

--- mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
+++ mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human> {
 
     private int wage;
     private String name;
@@ -21,5 +21,11 @@ public class Human {
     @Override
     public String toString() {
         return name + " " + wage;
+    }
+
+    @Override
+    public int compareTo(Human o) {
+        Human obj = (Human) o;
+        return obj.wage - this.wage;
     }
 }
