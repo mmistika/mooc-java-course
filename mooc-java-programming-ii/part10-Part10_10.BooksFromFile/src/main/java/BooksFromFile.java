466e25e44db527d7b6a21c346d650b92cf7cdbe9
mmistika <mistikasoft@gmail.com>
2025-02-15 16:27:51 +0100

    BooksFromFile solution

--- mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -7,11 +7,27 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
 
     }
 
+    public static List<Book> readBooks(String file) {
+        try {
+            return Files.lines(Paths.get(file))
+                    .map((String line) -> {
+                        String[] parts = line.split(",");
+                        String name = parts[0];
+                        int year = Integer.valueOf(parts[1]);
+                        int pages = Integer.valueOf(parts[2]);
+                        String author = parts[3];
+                        return new Book(name, year, pages, author);
+                    })
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
 }
