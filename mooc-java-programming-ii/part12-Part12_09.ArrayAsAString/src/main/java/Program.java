ae6c272154b95229a82969e4cf02d8fe803cbec8
mmistika <mistikasoft@gmail.com>
2025-02-25 15:34:35 +0100

    ArrayAsAString solution

--- mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -5,4 +5,16 @@ public class Program {
         // Test your method here
     }
 
+    public static String arrayAsString(int[][] array) {
+        StringBuilder builder = new StringBuilder();
+        
+        for (int i = 0; i < array.length; ++i) {
+            for (int j = 0; j < array[i].length; ++j) {
+                builder.append(array[i][j]);
+            }
+            builder.append("\n");
+        }
+        
+        return builder.toString();
+    }
 }
