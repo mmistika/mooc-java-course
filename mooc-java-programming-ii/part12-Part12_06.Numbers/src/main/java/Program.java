7908b3e337a0d882a4777ed8e86de8e4ae4fc61e
mmistika <mistikasoft@gmail.com>
2025-02-25 14:25:34 +0100

    Numbers solution

--- mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,14 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How many random numbers should be printed?");
+        int num = Integer.parseInt(scanner.nextLine());
+        
+        Random rand = new Random();
+        for (int i = 0; i < num; ++i) {
+            System.out.println(rand.nextInt(11));
+        }
     }
 
 }
