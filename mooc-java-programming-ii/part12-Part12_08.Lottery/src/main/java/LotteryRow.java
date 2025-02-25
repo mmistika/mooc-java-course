136f8d52486db49728ddbdf38e056fa7c1ec659a
mmistika <mistikasoft@gmail.com>
2025-02-25 15:28:32 +0100

    Lottery solution

--- mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -20,11 +20,18 @@ public class LotteryRow {
         this.numbers = new ArrayList<>();
         // Implement the random number generation here
         // the method containsNumber is probably useful
+        Random rand = new Random();
+        while (this.numbers.size() < 7) {
+            int num = rand.nextInt(40) + 1;
+            if (!this.containsNumber(num)) {
+                this.numbers.add(num);
+            }
+        }
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
-        return false;
+        return this.numbers.contains(number);
     }
 }
-
+
