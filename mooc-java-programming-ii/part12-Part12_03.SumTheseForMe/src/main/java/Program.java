895c27aa5092b0ce1d17e1c14abdaee1821612ba
mmistika <mistikasoft@gmail.com>
2025-02-25 11:12:24 +0100

    SumTheseForMe solution

--- mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Program {
@@ -6,7 +7,14 @@ public class Program {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // test your method here
-
     }
 
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+        fromWhere = Math.max(fromWhere, 0);
+        toWhere = Math.min(toWhere, array.length);
+
+        return Arrays.stream(array, fromWhere, toWhere)
+                .filter(num -> smallest <= num && num <= largest)
+                .sum();
+    }
 }
