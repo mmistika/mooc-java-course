240804751908b0c9c13abc4d3f665a507e5451a7
mmistika <mistikasoft@gmail.com>
2025-03-30 20:37:15 +0200

    ButtonAndLabel solution

--- mooc-java-programming-ii/part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
+++ mooc-java-programming-ii/part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -1,11 +1,28 @@
 package buttonandlabel;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndLabelApplication {
-
+public class ButtonAndLabelApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndLabelApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        FlowPane group = new FlowPane();
+        Scene view = new Scene(group);
+        
+        group.getChildren().add(new Label("Label"));
+        group.getChildren().add(new Button("Button"));
+        
+        stage.setScene(view);
+        stage.show();
     }
 
 }
