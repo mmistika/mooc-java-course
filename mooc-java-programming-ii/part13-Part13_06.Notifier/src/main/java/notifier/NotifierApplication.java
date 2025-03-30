b81358f67c6ee3ba494cd239aad61284ea4f6a0e
mmistika <mistikasoft@gmail.com>
2025-03-30 23:52:41 +0200

    NotifierApplication solution

--- mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,39 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.event.EventType;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
-
+public class NotifierApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        VBox box = new VBox();
+        Scene scene = new Scene(box);
+
+        TextField input = new TextField();
+        Button btn = new Button("Update");
+        Label label = new Label();
+
+        box.getChildren().add(input);
+        box.getChildren().add(btn);
+        box.getChildren().add(label);
+
+        btn.setOnAction((ev) -> {
+            label.setText(input.getText());
+        });
+
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
