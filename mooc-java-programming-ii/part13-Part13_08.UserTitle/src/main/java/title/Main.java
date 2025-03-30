94936d799d8abe3c76912355be34ecaefe32e64c
mmistika <mistikasoft@gmail.com>
2025-03-31 00:35:22 +0200

    UserTitle solution

--- mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,13 @@
 package title;
 
+import java.util.Scanner;
+import javafx.application.Application;
 
 public class Main {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner scan = new Scanner(System.in);
+        Application.launch(UserTitle.class, scan.nextLine());
     }
-
+    
 }
--- mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,14 @@
 package title;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class UserTitle {
+public class UserTitle extends Application {
 
-
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle(getParameters().getRaw().get(0));
+        stage.show();
+    }
+    
 }
