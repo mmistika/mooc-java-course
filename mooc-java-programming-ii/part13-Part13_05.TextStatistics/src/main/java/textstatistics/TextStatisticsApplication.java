92d57454ec67bc635dd4aac00ed6b203027125d3
mmistika <mistikasoft@gmail.com>
2025-03-30 22:51:23 +0200

    TextStatistics solution

--- mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,34 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
-
+public class TextStatisticsApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane);
+
+        pane.setCenter(new TextArea());
+        
+        HBox box = new HBox();
+        box.getChildren().add(new Label("Letters: 0"));
+        box.getChildren().add(new Label("Words: 0"));
+        box.getChildren().add(new Label("The longest word is:"));
+        pane.setBottom(box);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
