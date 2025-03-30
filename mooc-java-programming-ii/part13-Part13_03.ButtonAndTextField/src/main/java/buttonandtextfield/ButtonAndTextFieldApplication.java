3833a783f090f2b5b7191fae964df7b694a6f728
mmistika <mistikasoft@gmail.com>
2025-03-30 20:40:29 +0200

    ButtonAndTextField solution

--- mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,28 @@
 package buttonandtextfield;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndTextFieldApplication {
-
+public class ButtonAndTextFieldApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndTextFieldApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) {
+        FlowPane group = new FlowPane();
+        Scene view = new Scene(group);
+
+        group.getChildren().add(new Button("Button"));
+        group.getChildren().add(new TextField());
+
+        stage.setScene(view);
+        stage.show();
+    }
 }
