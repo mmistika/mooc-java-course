76a6124ff1b942739b98102de69e4357c4230582
mmistika <mistikasoft@gmail.com>
2025-04-15 01:15:29 +0200

    Database solution

--- mooc-java-programming-ii/part14-Part14_10.Database/src/main/java/application/UserInterface.java
+++ mooc-java-programming-ii/part14-Part14_10.Database/src/main/java/application/UserInterface.java
@@ -30,9 +30,68 @@ public class UserInterface {
             }
 
             // implement the functionality here
+            int option;
+            try {
+                option = Integer.parseInt(command);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+            switch (option) {
+                case 1:
+                    optList();
+                    break;
+                case 2:
+                    optAdd();
+                    break;
+                case 3:
+                    optMark();
+                    break;
+                case 4:
+                    optRemove();
+                    break;
+                default:
+                    break;
+            }
         }
 
         System.out.println("Thank you!");
     }
 
+    private void optList() throws SQLException {
+        System.out.println("Listing the database contents");
+        database.list().stream()
+                .forEach(t -> System.out.println(t));
+    }
+
+    private void optAdd() throws SQLException {
+        System.out.println("Adding a new todo");
+
+        System.out.println("Enter name");
+        String name = scanner.nextLine();
+        System.out.println("Enter description");
+        String desc = scanner.nextLine();
+        database.add(new Todo(name, desc, false));
+    }
+
+    private void optMark() throws SQLException {
+        System.out.println("Which todo should be marked as done (give the id)?");
+        int id;
+        try {
+            id = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return;
+        }
+        database.markAsDone(id);
+    }
+
+    private void optRemove() throws SQLException {
+        System.out.println("Which todo should be removed (give the id)?");
+        int id;
+        try {
+            id = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return;
+        }
+        database.remove(id);
+    }
 }
