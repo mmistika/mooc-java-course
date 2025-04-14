722dc87e7ba039ecc64c42ca2f47552ce509d6c7
mmistika <mistikasoft@gmail.com>
2025-04-14 23:42:19 +0200

    Hurray solution

--- mooc-java-programming-ii/part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
+++ mooc-java-programming-ii/part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
@@ -1,9 +1,11 @@
 package hurraa;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 
 public class HurraaSovellus extends Application {
@@ -15,7 +17,11 @@ public class HurraaSovellus extends Application {
         Button nappi = new Button("Hurraa!");
         pane.setCenter(nappi);
 
-
+        AudioClip audio = new AudioClip("file:Applause-Yannick_Lemieux.wav");
+        nappi.setOnAction((event) -> {
+            audio.play();
+        });
+        
         Scene scene = new Scene(pane, 600, 400);
 
         stage.setScene(scene);
